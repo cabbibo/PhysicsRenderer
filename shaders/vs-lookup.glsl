4f41b24d64c0197ea772f86b47b6bf29f4e6af61
Isaac Cohen <isaaclandoncohen@gmail.com>
2015-01-21 13:47:10 -0800

    adding new lookup

--- shaders/vs-lookup.glsl
+++ shaders/vs-lookup.glsl
@@ -4,6 +4,7 @@ void main(){
 
   vec4 pos = texture2D( t_pos , position.xy );
 
+  gl_PointSize = min( 5. ,  50. / length( dif ));
   gl_Position = projectionMatrix * modelViewMatrix * vec4( pos.xyz , 1. );
 
 
