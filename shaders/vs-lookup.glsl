33831db3431da861230c1a3ac1b115ad239d9150
Isaac Cohen <isaaclandoncohen@gmail.com>
2015-01-21 13:48:18 -0800

    fixing lookup

--- shaders/vs-lookup.glsl
+++ shaders/vs-lookup.glsl
@@ -4,6 +4,8 @@ void main(){
 
   vec4 pos = texture2D( t_pos , position.xy );
 
+  vec3 dif = cameraPosition - pos.xyz;
+  
   gl_PointSize = min( 5. ,  50. / length( dif ));
   gl_Position = projectionMatrix * modelViewMatrix * vec4( pos.xyz , 1. );
 
